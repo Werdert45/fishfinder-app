6b6c486018d7fbd4f7d3d3ec86b81c2742166ab2
Ian Ronk <ian.ronk0@gmail.com>
2020-02-08 18:24:54 +0100

    APP-17: able to send BASE64 encoded images to API

--- lib/screens/home/camera/camerapreview.dart
+++ lib/screens/home/camera/camerapreview.dart
@@ -42,38 +42,17 @@ class DisplayPictureScreen extends StatelessWidget {
 
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () async {
-//          var _image = File(imagePath);
-//          List<int> imageBytes = _image.readAsBytesSync();
-//          String base64Image = base64UrlEncode(imageBytes);
-//
-//          var uploadURL = 'http://192.168.2.21:5000/fishfinder/predict/';
-//
-//          Map<String, String> data = {'b64': base64Image};
-//
-//          Dio dio = new Dio();
-//
-//          dio.post(uploadURL, data:data);
 
           var _image = File(imagePath);
           List<int> imageBytes = _image.readAsBytesSync();
+          // Correctly encoded
           String base64Image = base64UrlEncode(imageBytes);
-          //TODO arrange the base64 as data = {'b64': base64Image}
-          // TODO send request: requests.post(url=URL, data=data
-          var httpClient = new http.Client();
           var uri = Uri.http("192.168.2.21:5000", "/fishfinder/predict/");
           Map<String, String> data = {'b64': base64Image};
-          http.Response response = await http.post(uri, body: data);
-          int statusCode = response.statusCode;
-          print("Hello");
-//
-//          uploadFile() async {
-//            var postUri = Uri.http("http://192.168.2.21:5000", "/fishfinder/predict/");
-//            var request = new http.MultipartRequest("POST", postUri);
-//            request.headers.addEntries(data);
-//
-//          }
-
-
+          print(data['b64'].length);
+          http.Response response = await http.post(uri, body:data);
+          var resp = response.body;
+          print(resp);
         },
         label: Row(
           children: <Widget>[Text("SCAN"), SizedBox(width: 10), Icon(Icons.done)],
