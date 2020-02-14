fa469f986b110750dd7d4e79ae62f18ac6f59fc7
Ian Ronk <ian.ronk0@gmail.com>
2020-02-14 20:20:00 +0100

    APP-19: setup all things to work on JSON experiment

---
+++ lib/screens/home/species/list.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:fishfinder_app/models/preview_species.dart';
+
+class SpeciesList extends StatelessWidget {
+  final List<previewSpecies> species;
+  SpeciesList({Key key, this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+        itemCount: species == null ? 0 : species.length,
+        itemBuilder: (BuildContext context, int index) {
+          return
+            new Card(
+              child: new Container(
+                child: new Center(
+                    child: new Column(
+                      // Stretch the cards in horizontal axis
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+
+                        new Text(
+                          // Read the name field value and set it in the Text widget
+                          species[index].number,
+                          // set some style to text
+                          style: new TextStyle(
+                              fontSize: 20.0, color: Colors.lightBlueAccent),
+                        ),
+                        new Text(
+                          // Read the name field value and set it in the Text widget
+                          "Name:- " + species[index].name,
+                          // set some style to text
+                          style: new TextStyle(
+                              fontSize: 20.0, color: Colors.amber),
+                        ),
+                      ],
+                    )),
+                padding: const EdgeInsets.all(15.0),
+              ),
+            );
+        });
+  }
+}
